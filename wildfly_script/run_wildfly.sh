8fa4fc8db51fcd8d6d072114b6f91bd8b7e0607e
suryogumilar <suryo.gumilar@gmail.com>
2023-03-25 21:00:41 -0700

    since envi not set automatically although already reside in /etc/profile.d we put it in run_wildfly.sh

--- wildfly_script/run_wildfly.sh
+++ wildfly_script/run_wildfly.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+source /etc/profile.d/java.sh
+source /etc/profile.d/ant.sh
 source /etc/profile.d/wildfly_setenv.sh
 /opt/wildfly/bin/launch.sh $WILDFLY_MODE $WILDFLY_CONFIG $WILDFLY_BIND
